d5ed1f0fd4c57b064521f68f577222eeafa23fa6
Alberto Di Biase <asdibiase@uc.cl>
2025-12-29 01:56:43 -0300

    Add segments to RDP algorithm

--- src/st/simplify_tract.pyx
+++ src/st/simplify_tract.pyx
@@ -1,42 +1,95 @@
+import cython
+from libc.math cimport sqrt, abs
+
 import numpy as np
 cimport numpy as cnp
 
 cnp.import_array()
 
+cdef class Segment:
+    cdef Segment seg_r, seg_l
+    cdef double[:, :] data
+
+    def __cinit__(self, Segment seg_r = None, Segment seg_l = None, double[:, :] data = None):
+        self.seg_r = seg_r
+        self.seg_l = seg_l
+        self.data = data
+
+    cdef set_data(self, double[:, :] data):
+        self.data = data
+
+    cpdef bint is_leaf(self):
+        return self.seg_r is None and self.seg_l is None
+
+    cdef set_children(self, Segment seg_r, Segment seg_l):
+        self.seg_r = seg_r
+        self.seg_l = seg_l
 
 
-cdef cnp.ndarray[cnp.float64_t, ndim=2] simplify_tract_imp(cnp.ndarray[cnp.float64_t, ndim=2] tract, double tolerance=1.0):
+    cpdef double[:, :] concatenate_segments(self):
+
+        if self.is_leaf():
+            return self.data
+
+        d_r = self.seg_r.concatenate_segments()
+        d_l = self.seg_l.concatenate_segments()
+
+        cdef double[:, :] d = np.empty((d_r.shape[0] + d_l.shape[0], d_r.shape[1]), dtype=np.float64)
+        d[: d_r.shape[0], :] = d_r
+        d[d_r.shape[0]:, :] = d_l
+        return d
+
+
+@cython.boundscheck(False) # turn off bounds-checking for entire function
+@cython.wraparound(False)  # turn off negative index wrapping for entire function
+cdef tuple[double, int] max_abs_dist(double[:, :] points, double[:] start, double[:] line_unit) noexcept:
+    cdef int n = points.shape[0]
+    cdef int i
+    cdef int argmax = 0
+    cdef double max_val = 0.0
+    cdef double cross
+    for i in range(n):
+        cross = abs((points[i, 0] - start[0]) * line_unit[1] - (points[i, 1] - start[1]) * line_unit[0])
+        if cross > max_val:
+            max_val = cross
+            argmax = i
+    return max_val, argmax
+
+
+@cython.boundscheck(False) # turn off bounds-checking for entire function
+@cython.wraparound(False)  # turn off negative index wrapping for entire function
+@cython.cdivision(True)    # enable C division semantics for entire function
+cdef Segment simplify_tract_imp(Segment tract, double tolerance=1.0) noexcept:
     # Simplify the tract using the Ramer-Douglas-Peucker algorithm
 
-    if len(tract) < 3:
+    cdef double[:, :] pts = tract.concatenate_segments()
+    if pts.shape[0] < 3:
         return tract
 
     # Find the point with the maximum distance from the line
-    cdef cnp.ndarray[cnp.float64_t, ndim=1] start, end
-    start, end = tract[0], tract[-1]
-    cdef cnp.ndarray[cnp.float64_t, ndim=1] line_vec = end - start
-    cdef double line_len = np.linalg.norm(line_vec)
+    cdef double[:] start, end
+    start, end = pts[0], pts[pts.shape[0]-1]
+    cdef double[2] line_vec = [end[0] - start[0], end[1] - start[1]]
+    cdef double line_len = sqrt(line_vec[0]**2 + line_vec[1]**2)
     if line_len < 1e-6:
-        return np.array([start])
+        return Segment(data=np.array([start]))
 
-    cdef cnp.ndarray[cnp.float64_t, ndim=1] line_unit = line_vec / line_len
-    diff = tract - start
-    cdef cnp.ndarray[cnp.float64_t, ndim=1] cross = diff[:, 0] * line_unit[1] - diff[:, 1] * line_unit[0]
-    cdef cnp.ndarray[cnp.float64_t, ndim=1] distances = np.abs(cross)  # Perpendicular distance
-    cdef double max_dist = np.max(distances)
+    cdef double[2] line_unit = [line_vec[0] / line_len, line_vec[1] / line_len]
+    cdef double max_dist
+    cdef int split_idx
+    max_dist, split_idx = max_abs_dist(pts, start, line_unit)  # Perpendicular distance
 
     if max_dist < tolerance:
-        return np.vstack([start, end])
+        return Segment(data=np.array([start, end]))
 
     # Recursively simplify the segments
-    cdef int split_idx = np.argmax(distances)
-    cdef cnp.ndarray[cnp.float64_t, ndim=2] left = simplify_tract_imp(tract[: split_idx + 1], tolerance)
-    cdef cnp.ndarray[cnp.float64_t, ndim=2] right = simplify_tract_imp(tract[split_idx:], tolerance)
-    return np.vstack([left[:-1], right])
+    cdef Segment left = simplify_tract_imp(Segment(data=pts[: split_idx + 1]), tolerance)
+    cdef Segment right = simplify_tract_imp(Segment(data=pts[split_idx:]), tolerance)
+    return Segment(left, right)
 
 
 def simplify_tract(cnp.ndarray[cnp.float64_t, ndim=2] tract, double tolerance=1.0):
     # Simplify the tract using the Ramer-Douglas-Peucker algorithm
 
-    cdef cnp.ndarray[cnp.float64_t, ndim=2] simplified = simplify_tract_imp(tract, tolerance)
-    return np.array(simplified)
+    cdef Segment simplified = simplify_tract_imp(Segment(data=tract), tolerance)
+    return np.array(simplified.concatenate_segments())
